dfc00ff3f76198f1c280e59f08c1c487b77b41ff
frezno <freznoshop@frezno.de>
2017-04-02 20:02:36 +0200

    Move public css and js files to /assets directory and update according paths

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,6 @@ require('laravel-elixir-vue-2');
  */
 
 elixir((mix) => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+    mix.sass('app.scss', 'public/assets/css/app.css')
+       .webpack('app.js', 'public/assets/js/app.js');
 });
