7782e9b5749725fe90afbb70e2d290b284e0ef28
frezno <freznoshop@frezno.de>
2017-04-04 23:08:18 +0200

    Installation of FontAwesome

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ require('laravel-elixir-vue-2');
  */
 
 elixir((mix) => {
+    mix.copy('node_modules/font-awesome/fonts', 'public/assets/fonts');
     mix.sass('app.scss', 'public/assets/css/app.css')
        .webpack('app.js', 'public/assets/js/app.js');
 });
