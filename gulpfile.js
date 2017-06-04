72acb15e456323db6c239a28b1887c94e7fa1029
frezno <freznoshop@frezno.de>
2017-06-04 15:39:38 +0200

    Add Bootstraps Glyphicons to the fonts

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,9 @@ const elixir = require('laravel-elixir');
 
 require('laravel-elixir-vue-2');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for your application as well as publishing vendor resources.
- |
- */
-
 elixir((mix) => {
-    mix.copy('node_modules/font-awesome/fonts', 'public/assets/fonts');
+    mix.copy('node_modules/font-awesome/fonts', 'public/assets/fonts')
+       .copy('node_modules/bootstrap-sass/assets/fonts', 'public/assets/fonts');
     mix.sass('app.scss', 'public/assets/css/app.css')
        .webpack('app.js', 'public/assets/js/app.js');
 });
