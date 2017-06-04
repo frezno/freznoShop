fded5463ff527bc0ef0d2ec4b6e1718be9a4f7c0
frezno <freznoshop@frezno.de>
2017-06-04 15:46:57 +0200

    Add query for all categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -13,18 +13,38 @@ class Category extends Model
     public $timestamps = false;
 
     /**
-     * Get all categories of the selected parent id.
+     * The attributes that are not mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * Get all categories.
      *
      * @param  int $parent_id  id of categories.
      * @param  int $group      id of category group.
+     * @param  bool $active    record is active or not.
      * @return array
      */
-    public static function getCategories($parent_id, $group)
+    public static function getCategories($parent_id, $group, $active = 1)
     {
-        return self::where('parent_id', '=', $parent_id)
-                        ->where('c_group', '=', $group)
-                        ->where('active', '=', 1)
-                        ->orderBy('sortorder')
-                        ->get();
+        //-- This query is used for navigation
+        //-- to get just the active entries.
+        if ($active === 1) {
+            return static::where('parent_id', '=', $parent_id)
+                            ->where('c_group', '=', $group)
+                            ->where('active', '=', 1)
+                            ->orderBy('sortorder')
+                            ->get();
+        } 
+         //-- Whereas this query is used in the Admin area
+         //-- to get ALL entries instead, active or not.
+         else {
+            return static::where('parent_id', '=', $parent_id)
+                            ->where('c_group', '=', $group)
+                            ->orderBy('sortorder')
+                            ->get();
+        }
     }
 }
