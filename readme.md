94511079f1d8f1c8844a61318eecde58e66e74a3
frezno <dev@frezno.de>
2018-11-13 21:02:57 +0100

    Restart from scratch, using Laravel 5.7

--- readme.md
+++ readme.md
@@ -6,12 +6,6 @@
 FreznoShop is an ecommerce / online shop software built using the fantastic [Laravel web application framework](https://github.com/laravel/framework).
 
 &nbsp;
-### Learning building FreznoShop
-
-A step-by-step tutorial will be available on the [FreznoShop homepage](http://freznoshop.de).  
-This tutorial will be updated while we are creating and extending the shop, please bear this in mind.
-
-&nbsp;
 ### License
 
 FreznoShop is open-source and licensed under the [MIT license](http://opensource.org/licenses/MIT).
--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,16 @@
 <?php
 
-Auth::routes();
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-//-- Home Sweet Home
-Route::get('/', 'HomeController@index');
-
-//-- Static pages, just one page per topic
-Route::get('about_us', function () { return view('pages.about'); });
-Route::get('contact_us', function () { return view('pages.contact'); });
-Route::get('terms_and_conditions', function () { return view('pages.terms'); });
-
-//-----------------------------------------------------------------------------
-//-- Admin
-Route::get('admin', '\App\Backend\Admin\Controllers\DashboardController');
-Route::resource('admin/categories', '\App\Backend\Admin\Controllers\CategoriesController', ['except' => ['show', 'create']]);
-
-//-----------------------------------------------------------------------------
-//-- Products
-//-- Calling the categories and products by using a slug
-//-- the long way would be eg:
-//Route::get('{slug}', 'ProductsController@getProductsByCategorySlug')->where('slug', '[a-z0-9-/]+');
-Route::pattern('slug', '[a-z0-9-/.]+');
-
-//-- The individual product (+ children) gets called with the product/ prefix
-Route::get('product/{slug}', function () { return view('shop.product_details'); });
-
-//-- All products of a certain category will be called by just the category slug
-Route::get('{slug}', 'Shop\ProductsController@getProductsByCategorySlug');
+Route::get('/', function () {
+    return view('welcome');
+});
