70e1d882770485ec944f9eb23060ca0c367f8db8
frezno <dev@frezno.de>
2018-12-23 20:02:05 +0100

    Add routes of footer pages

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,14 @@
 //-- Home Sweet Home
 Route::get('/', 'HomeController@index');
 
+//-- Static pages, just one page per topic
+Route::view('faq', 'pages.faq');
+Route::view('terms', 'pages.terms');
+Route::view('aboutus', 'pages.aboutus');
+Route::view('privacy', 'pages.privacy');
+Route::view('shipping', 'pages.shipping');
+Route::view('legalinfo', 'pages.legalinfo');
+
 //-----------------------------------------------------------------------------
 //-- Authentication rulez - but disable basic registration
 Auth::routes(['register' => false]);
