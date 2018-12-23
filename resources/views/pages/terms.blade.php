3afa5df538db49c4466509d20cdfbcf8aeda9e8d
frezno <dev@frezno.de>
2018-12-23 20:02:26 +0100

    Add static footer pages

---
+++ resources/views/pages/terms.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="px-4 py-2">
+    <div class="xl:container mx-auto border border-green-darker p-4">
+        <h2 class="text-center mb-6">Terms &amp; Conditions</h2>
+
+    </div>
+</div>
+@endsection
