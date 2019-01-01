37e4bdcdc3ea5e5ba3c42680d2b183fbd40e6e62
frezno <dev@frezno.de>
2019-01-01 18:35:56 +0100

    Add simple image slider

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,19 @@
 @extends('layouts.master')
 
 @section('content')
+<!-- Saying hello to the user -->
+<div class="container mx-auto">
+    <div class="m-4 text-center">
+        <h1>Welcome to FreznoShop</h1>
+        <small>Shop until you drop</small>
+    </div>
+</div>
+
+<!-- Let's get started -->
 <div class="flex flex-col md:flex-row mb-4">
 
     <!-- Newsticker -->
-    <div class="flex flex-col md:w-1/2 lg:w-1/3 px-4 newsTicker">
+    <div class="flex flex-col md:w-1/2 px-4 newsTicker">
 
         <!-- Any News? -->
         @if (count($news) > 0)
@@ -35,11 +44,21 @@
         @endif
     </div>
 
-    <!-- Info/Slider -->
-    <div class="flex flex-col md:w-1/2">
-        <div class="pt-10 text-center">
-            <h1>Welcome to FreznoShop</h1>
-            <small>Shop until you drop</small>
+    <!-- Image-Slider -->
+    <div class="flex flex-col md:w-1/2 mx-auto px-4">
+        <div class="hidden sm:block items-center mb-2 mt-4 mx-auto picSlider">
+            <img src="{{ url('/images/promo/freestyle.png') }}" class="rotate" alt="">
+            <img src="{{ url('/images/promo/bikes.jpg') }}" class="rotate" alt="">
+            <img src="{{ url('/images/promo/race.jpg') }}" class="rotate" alt="">
+            <img src="{{ url('/images/promo/mtb.jpg') }}" class="rotate" alt="">
+        </div>
+    </div>
+</div>
+
+<div class="flex flex-col md:flex-row border-t-2 mb-4">
+    <div class="container mx-auto">
+        <div class="mt-4 text-center">
+            <h2>Sneak Preview</h2>
         </div>
     </div>
 </div>
@@ -55,18 +74,42 @@
           slides[i].setAttribute("style", "display:none");
         }
 
-        count++;
+        count1++;
 
-        if (count > slides.length) {
-            count = 1;
+        if (count1 > slides.length) {
+            count1 = 1;
         }
 
-        slides[count-1].setAttribute("style", "display:block");
+        slides[count1-1].setAttribute("style", "display:block");
 
         setTimeout(newsTicker, 7200);
     }
 
-    var count = 0;
+    var count1 = 0;
     newsTicker();
 </script>
+
+<script>
+        var picSlider = function () {
+            var j;
+            var slides = document.querySelectorAll(".rotate");
+
+            for (j = 0; j < slides.length; j++) {
+              slides[j].setAttribute("style", "display:none");
+            }
+
+            count2++;
+
+            if (count2 > slides.length) {
+                count2 = 1;
+            }
+
+            slides[count2-1].setAttribute("style", "display:block");
+
+            setTimeout(picSlider, 3600);
+        }
+
+        var count2 = 0;
+        picSlider();
+    </script>
 @endsection
