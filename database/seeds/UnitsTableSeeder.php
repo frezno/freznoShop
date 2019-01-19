edcf911dd97bdbabe170b507bc274500f81df21c
frezno <dev@frezno.de>
2019-01-19 17:48:27 +0100

    Add seed units

---
+++ database/seeds/UnitsTableSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UnitsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $units = [
+            [
+                'uom' => 'pc',
+                'description' => 'Piece(s)'
+            ],
+            [
+                'uom' => 'pr',
+                'description' => 'Pair(s)'
+            ],
+            [
+                'uom' => 'kit',
+                'description' => 'Kit(s)'
+            ],
+            [
+                'uom' => 'set',
+                'description' => 'Set(s)'
+            ],
+            [
+                'uom' => 'oz',
+                'description' => 'Ounces'
+            ],
+            [
+                'uom' => 'lb',
+                'description' => 'Pound(s)'
+            ],
+            [
+                'uom' => 'gal',
+                'description' => 'Gallon'
+            ],
+        ];
+    }
+}
