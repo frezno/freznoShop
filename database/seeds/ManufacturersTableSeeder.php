b403463ce27c5dabb0dd38c3755e420055208507
frezno <dev@frezno.de>
2019-01-19 17:47:34 +0100

    Add seed manufacturers

---
+++ database/seeds/ManufacturersTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ManufacturersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $manufacturers = [
+            [
+                'name' => 'Thor',
+                'logo' => 'thor_logo.jpg'
+            ],
+            [
+                'name' => 'Alpinestars'
+            ],
+            [
+                'name' => 'Fox'
+            ],
+            [
+                'name' => 'Split'
+            ],
+            [
+                'name' => 'Mitas',
+                'logo' => 'mitas_logo.jpg'
+            ],
+            [
+                'name' => 'Putoline',
+                'logo' => 'putoline_logo.jpg'
+            ],
+            [
+                'name' => 'SKF',
+                'logo' => 'skf_logo'
+            ],
+        ];
+    }
+}
