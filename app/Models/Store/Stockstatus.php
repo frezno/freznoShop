a51305ff105d7df8562e8334e85ee279ddf7bed0
frezno <dev@frezno.de>
2019-01-19 17:43:51 +0100

    Add model Stockstatus

---
+++ app/Models/Store/Stockstatus.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Modes\Store;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Stockstatus extends Model
+{
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
