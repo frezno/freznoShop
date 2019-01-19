f32d810605f9ccb453df14f41d8eaa71a0b7bb19
frezno <dev@frezno.de>
2019-01-19 17:43:18 +0100

    Add model ProductGroup

---
+++ app/Models/Store/ProductGroup.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Store;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductGroup extends Model
+{
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
