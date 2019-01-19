0ee47691b0172853d61ba84160045c1c5c6d28a5
frezno <dev@frezno.de>
2019-01-19 17:51:39 +0100

    Add table ProductGroups

---
+++ database/migrations/2019_01_14_215007_create_product_groups_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_groups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->boolean('teaser')->default(false);
+            $table->boolean('recommended')->default(false);
+            $table->boolean('active')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_groups');
+    }
+}
