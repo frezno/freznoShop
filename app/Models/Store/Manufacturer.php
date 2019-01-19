1ece36e76c8b5ea50a6f308bd856e1fe231aaf9f
frezno <dev@frezno.de>
2019-01-19 17:42:12 +0100

    Add table Manufacturer

---
+++ app/Models/Store/Manufacturer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Store;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Manufacturer extends Model
+{
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
