6e778475a544e47348248ddafe0aff01685698a2
frezno <dev@frezno.de>
2019-02-01 23:26:46 +0100

    Add some more categories

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -40,6 +40,97 @@ class CategoriesTableSeeder extends Seeder
                 'slug' => 'news',
                 'sortorder' => 4
             ],
+            [
+                'parent_id' => 1,
+                'parent_group_id' => 1,
+                'name' => 'Jerseys',
+                'slug' => 'jerseys',
+                'sortorder' => 1
+            ],
+            [
+                'parent_id' => 1,
+                'parent_group_id' => 1,
+                'name' => 'Pants',
+                'slug' => 'pants',
+                'sortorder' => 2
+            ],
+            [
+                'parent_id' => 1,
+                'parent_group_id' => 1,
+                'name' => 'Gloves',
+                'slug' => 'gloves',
+                'sortorder' => 3
+            ],
+            [
+                'parent_id' => 1,
+                'parent_group_id' => 1,
+                'name' => 'Boots',
+                'slug' => 'boots',
+                'sortorder' => 4
+            ],
+            [
+                'parent_id' => 1,
+                'parent_group_id' => 2,
+                'name' => 'Suspension',
+                'slug' => 'suspension',
+                'sortorder' => 1
+            ],
+            [
+                'parent_id' => 1,
+                'parent_group_id' => 2,
+                'name' => 'Tires',
+                'slug' => 'tires',
+                'sortorder' => 2
+            ],
+            [
+                'parent_id' => 1,
+                'parent_group_id' => 2,
+                'name' => 'Oil and Grease',
+                'slug' => 'oil-and-grease',
+                'sortorder' => 3
+            ],
+            [
+                'parent_id' => 1,
+                'parent_group_id' => 3,
+                'name' => 'T-Shirts',
+                'slug' => 't-shirts',
+                'sortorder' => 1
+            ],
+            [
+                'parent_id' => 1,
+                'parent_group_id' => 3,
+                'name' => 'Caps',
+                'slug' => 'caps',
+                'sortorder' => 2
+            ],
+            [
+                'parent_id' => 8,
+                'parent_group_id' => 1,
+                'name' => 'Offroad',
+                'slug' => 'offroad-boots',
+                'sortorder' => 1
+            ],
+            [
+                'parent_id' => 8,
+                'parent_group_id' => 1,
+                'name' => 'Street',
+                'slug' => 'onroad-boots',
+                'sortorder' => 2
+            ],
+            [
+                'parent_id' => 10,
+                'parent_group_id' => 2,
+                'name' => 'Offroad',
+                'slug' => 'offroad-tires',
+                'sortorder' => 1
+            ],
+            [
+                'parent_id' => 10,
+                'parent_group_id' => 2,
+                'name' => 'Street',
+                'slug' => 'onroad-tires',
+                'sortorder' => 2
+            ],
         ];
 
         DB::table('categories')->insert($categories);
